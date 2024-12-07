c15c298a5cc2a94b50617d7611d2ec9985c0e409
S1A2M3I <108212900+S1A2M3I@users.noreply.github.com>
2024-12-07 16:41:47 +0530

    Changes

--- Fundamental.js
+++ Fundamental.js
@@ -69,3 +69,150 @@ let person = {
     city: "New York"
 };
 console.log(person);
+
+//Basic Operators
+//Arithmetic Operators
+//Logical Operators
+//Comparison Operators
+//Assignment Operators
+//Bitwise Operators
+//Ternary Operator
+//Relational Operators
+
+//Mathematical Operators 
+console.log(10+20);
+console.log(10-20);
+console.log(10*20);
+console.log(10/20);
+console.log(10%20);
+console.log(10**20);
+
+//Assignment Operators
+let x = 10;
+console.log(x);
+x+=20;
+console.log(x);
+x-=20;
+console.log(x);
+x*=20;
+console.log(x);
+x/=20;
+console.log(x);
+x%=20;
+console.log(x);
+x**=20;
+console.log(x);
+
+//Logical Operators
+//&&, ||, !
+
+//Comparison Operators
+console.log(10==20);
+console.log(10!=20);
+console.log(10>20);
+console.log(10<20);
+console.log(10>=20);
+console.log(10<=20);
+console.log(10===20);
+console.log("5" == 5);
+console.log("5" === 5);
+
+let currentyears = 2024;
+console.log(currentyears+=10 < (currentyears+25));
+//operator precedence
+//as first it evaluates (currentyears+=10) then it evaluates (currentyears+25) then it evaluates (currentyears+=10 < 
+//(currentyears+25))
+//then  it evaluates (currentyears+=10 < (currentyears+25)) which is true and returns true
+//then the value of true is 1 and then (currentyears+=1) is 2024+1 which is 2025)
+
+
+//String and String Template literals
+const job="trainer";
+const name="John";
+const dob=2000;
+
+//"" and + are used for concatenation
+console.log("My name is "+name+" and my job is "+job+" and my dob is "+dob);
+//`` is used for string template literals
+console.log(`My name is ${name} and my job is ${job} and my dob is ${dob}`);
+
+//Conditional Statements
+let age = 20;
+if(age>=18){
+    console.log("Elgible for driving license");
+}
+else{
+    console.log("Not elgible for driving license");
+}
+
+
+//multiple if statements
+let age3 = 20;
+if(age3>=18){
+    console.log("Elgible for driving license");
+}
+if(age3>=21){
+    console.log("Elgible for voting");
+}
+else{
+    console.log("Not elgible for driving license");
+    console.log("Not elgible for voting");
+}
+
+//nested if
+let age1 = 20;
+if(age1>=18){
+    console.log("Elgible for driving license");
+    if(age1>=21){
+        console.log("Elgible for voting");
+    }
+}
+else{
+    console.log("Not elgible for driving license");
+    if(age1<18){
+        console.log("Not elgible for voting");
+    }
+}
+
+//else-if ladder
+let age2 = 20;
+if(age2>=18){
+    console.log("Elgible for driving license");
+}
+else if(age2>=21){
+    console.log("Elgible for voting");
+}
+else{
+    console.log("Not elgible for driving license");
+    console.log("Not elgible for voting");
+}
+
+//break
+//continue
+//return
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+    if(i==5){
+        break;
+    }
+}
+
+let j=0;
+while(j<10){
+    j++;
+    if(j==5){
+        continue;
+    }
+    console.log(j);
+}
+
+let k=0;
+while(k<10){
+    k++;
+    if(k==5){
+        return;
+    }
+    console.log(k);
+}
