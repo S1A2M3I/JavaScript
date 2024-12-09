e781b11dba8a67d85a931e3bfd56afe43a7b3acc
S1A2M3I <108212900+S1A2M3I@users.noreply.github.com>
2024-12-09 09:04:47 +0530

    Chanegs

--- Fundamental.js
+++ Fundamental.js
@@ -298,3 +298,30 @@ for(let i=0;i<numbers.length;i++){
     console.log(numbers[i]+" is prime numbers");
     }
 }
+
+//Pallindrome
+const num = "121";
+let rev = "";
+for(let i = num.length-1;i>=0;i--){
+    rev = rev + num[i];
+}
+if(rev==num){
+    console.log("It is a palindrome");
+}
+else{
+    console.log("It is not a palindrome");
+    }
+
+
+//map(), reduce(), filter()
+const numbers2 = [1,2,3,4,5,6,7,8,9,10];
+//map() The map() method is used for creating a new array from an existing one, applying a function to each one of the 
+//elements of the first array.
+const square = numbers2.map((num) => num*num);
+console.log(square);
+//reduce() The reduce() method reduces an array of values down to just one value.
+const sum = numbers2.reduce((acc,num) => acc+num,0);
+console.log(sum);
+//filter() The filter() method takes each element in an array and it applies a conditional statement against it. 
+const even = numbers2.filter((num) => num%2==0);
+console.log(even);
