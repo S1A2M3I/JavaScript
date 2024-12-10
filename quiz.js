e51db82718dc8ecf0f6340b0b16417d95ec84134
S1A2M3I <108212900+S1A2M3I@users.noreply.github.com>
2024-12-10 13:02:19 +0530

    Changes

--- quiz.js
+++ quiz.js
@@ -68,14 +68,64 @@ function showquestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
+
+        if (answer.correct === true) {
+            button.dataset.correct = answer.correct;
+        }
+
+        button.addEventListener("click", (selectAnswer));
     })
 }
 
 function resetState() {
+    nextButton.style.display = "none";
     while (answerButton.firstChild) {
         answerButton.removeChild(answerButton.firstChild);
-    }    
+    }
 }
 
-//answerButton.firstChild is used to remove the first child of the answerButton element.
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    if (correct) {
+        selectedButton.classList.add("correct");
+        score++;
+    } else {
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach((button) => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showquestion();
+    }
+    else {
+        showScore();
+
+    }
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();    
+    }
+})
+
 startQuiz();
